096357e9df7530065cc121de1931365b0ddd1e4a
Sean Berrios <722320@student.dusd.net>
2023-11-08 11:42:10 -0800

    made my character move
    I misspelled the vertical word

--- RubysAdventureBerriosP3/Assets/scripts/RudyController.cs
+++ RubysAdventureBerriosP3/Assets/scripts/RudyController.cs
@@ -8,18 +8,17 @@ public class RudyController : MonoBehaviour
     void Start()
     {
         
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Veritcal");
+        float vertical = Input.GetAxis("Vertical");
 
         Vector2 postion = transform.position;
-        postion.x = postion.x + 0.1f * horizontal * Time.deltaTime;
-        postion.y = postion.y + 0.1f * vertical * Time.deltaTime;
+        postion.x = postion.x + 3.0f * horizontal * Time.deltaTime;
+        postion.y = postion.y + 3.0f * vertical * Time.deltaTime;
 
         transform.position = postion;
     }
